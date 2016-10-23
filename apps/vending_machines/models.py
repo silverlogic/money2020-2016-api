ea56535033caef70efa7ba6d975d04232eaf2430
Ryan Pineo <ryanpineo@gmail.com>
2016-10-23 05:51:42 -0700

    add string representations to vending machine and product

--- apps/vending_machines/models.py
+++ apps/vending_machines/models.py
@@ -7,6 +7,9 @@ class VendingMachine(models.Model):
     name = models.CharField(max_length=100)
     serial = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     machine = models.ForeignKey('VendingMachine')
@@ -14,3 +17,6 @@ class Product(models.Model):
     icon = models.ImageField(upload_to=random_name_in('product-icons'))
     price = models.DecimalField(max_digits=10, decimal_places=2)
     product_identifier = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
